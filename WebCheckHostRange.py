622fc811a1ef697ad87c93aad852172d58445efe
humanist <humanist@example.org>
2021-12-07 18:46:00 +0100

    Adding catch of erros and timeout to request

--- WebCheckHostRange.py
+++ WebCheckHostRange.py
@@ -15,8 +15,11 @@ def main():
         headers = {'Host':host}
         for ip in ipaddress.IPv4Network(range, False):
             print("Checking address:", ip)
-            r = requests.get("http://"+str(ip)+":"+port,headers=headers)
-            print(r.text)
+            try:
+                r = requests.get("http://"+str(ip)+":"+port,headers=headers, timeout=2)
+                print(r.text)
+            except:
+                print("This address doesn't seem to work")
         
     else:
         print("Please launch the software using this command scheme: ./WebCheckHostRange.py 127.0.0.1/32 google.com 80")
