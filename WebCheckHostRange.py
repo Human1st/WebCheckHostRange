a56b3507d33bf0657a7abc7a41e1d1dc9d7c3e2b
humanist <humanist@example.org>
2021-12-08 11:17:00 +0100

    Adding keyword to match when scanning

--- WebCheckHostRange.py
+++ WebCheckHostRange.py
@@ -10,9 +10,11 @@ def main():
     parser.add_argument("--iprange", "-r", type=str, nargs="+", help="The ip range that we will search in", required=True)
     parser.add_argument('--host', '-i', type=str, nargs="+", help="Host to search for", required=True)
     parser.add_argument('--port', '-p', type=int, nargs="+", help="Port to use", default=80)
+    parser.add_argument('--keyword', '-k', type=str, nargs="+", help="Keyword to validate the page", required=True)
     args = parser.parse_args()
     range = str(args.iprange[0])
     host = str(args.host[0])
+    keyword = str(args.keyword[0])
     port = args.port
     print("Range to scan:", range)
     print("Host to check:", host)
@@ -22,7 +24,8 @@ def main():
         print("Checking address:", ip)
         try:
             r = requests.get("http://"+str(ip)+":"+str(port),headers=headers, timeout=2)
-            print(r.text)
+            if keyword in r.text:
+                print("Potential match with:","http://"+str(ip)+":"+str(port))
         except:
             print("This address doesn't seem to work")
 
